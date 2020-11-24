f7cf9d7506136969689095bb9a35e0c58c9715c3
Nico Bruenjes <nico.bruenjes@gmail.com>
2020-11-25 00:16:54 +0100

    six feet under
    where we add markdown-it

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,28 @@
 const markdownIt = require( "markdown-it" );
 const markdownItAnchor = require( "markdown-it-anchor" );
+const markdownItContainer = require('markdown-it-container');
+const markdownItAttrs = require('markdown-it-attrs');
 
 module.exports = function( eleventyConfig ) {
   eleventyConfig.addLayoutAlias( "artikel", "layouts/posts.njk" );
+
+  let markdownItOptions = {
+    html: true,
+    linkify: true,
+    typographer: true
+  };
+
+  let markdownLib = markdownIt(markdownItOptions)
+    .use(markdownItContainer, 'warning' )
+    .use(markdownItContainer, 'tip' )
+    .use(markdownItAttrs, {
+      leftDelimiter: '{:',
+      rightDelimiter: '}',
+      allowedAttributes: ['id', 'class', /^data\-.*$/],
+    });
+
+  eleventyConfig.setLibrary('md', markdownLib);
+
   return {
     dir: {
       input: 'src',
